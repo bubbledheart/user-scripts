6b57030287ab10e1c4c623fc4884ea401d01a015
mpeter <martin.mpeter@gmail.com>
2025-02-01 11:45:08 +0100

    add reject_youtube_cookies

---
+++ reject_youtube_cookies.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         reject_youtube_cookies
+// @version      1.0
+// @description  This script rejects all Youtube cookies
+// @match        *.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+const maxRetries = 5;
+
+(function() {
+    'use strict';
+
+    const scriptName = GM_info.script.name;
+    let rejectButton;
+    let counter = 0;
+
+    function rejectCookies() {
+        if(!rejectButton) {
+            const span = Array.from(document.querySelectorAll('span'))
+            .find(el => el.textContent.trim() === 'Reject all' || el.textContent.trim() === 'Alle ablehnen');
+
+            rejectButton = span?.closest('button');
+            if(rejectButton) {
+                console.log(scriptName, ": rejectButton found", rejectButton);
+            }
+        }
+
+        if(rejectButton) {
+            rejectButton.click();
+            console.log(scriptName, ": rejected cookies");
+        }
+
+        if(counter < maxRetries) {
+            setTimeout(rejectCookies, 1000);
+            counter++;
+        }
+    }
+
+    setTimeout(rejectCookies, 1000);
+})();
