58ae8c93b2755304ce22ad31edd7df53a0a4f5ed
mpeter <martin.mpeter@gmail.com>
2025-02-01 11:42:05 +0100

    add annotation script

---
+++ disable_youtube_annotations.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         disable_youtube_annotations
+// @version      1.0
+// @description  This script turns off Youtube's annotations
+// @match        *.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const scriptName = GM_info.script.name;
+    let annotationsToggle;
+    let settingsInitialized;
+
+    function uncheck(toggle) {
+        if (toggle && toggle.getAttribute('aria-checked') === 'true') {
+            toggle.click();
+            console.log(scriptName, ": unchecked", toggle);
+        }
+    }
+
+    function disableAfterLoad() {
+        if(!settingsInitialized) {
+            let settings_button = document.querySelector(".ytp-settings-button");
+            if(settings_button) {
+                settings_button.click();
+                settings_button.click();
+
+                let all_labels = document.querySelectorAll(".ytp-menuitem-label");
+                if(all_labels.length > 0) {
+                    settingsInitialized = true;
+                    console.log(scriptName, ": settings initialized");
+                }
+
+                for (let i = 0; i < all_labels.length; i++) {
+                    if (all_labels[i].innerHTML == "Annotations") {
+                        annotationsToggle = all_labels[i].parentNode;
+                        console.log(scriptName, ": annotationsToggle found", annotationsToggle);
+                    }
+                }
+            }
+            if(!annotationsToggle) {
+                console.log(scriptName, ": no annotationsToggle found");
+                return;
+            }
+        }
+
+        if(annotationsToggle) {
+            uncheck(annotationsToggle);
+        }
+                console.log(scriptName, ": test");
+
+        setTimeout(disableAfterLoad, 5000);
+    }
+
+    setTimeout(disableAfterLoad, 5000);
+})();
