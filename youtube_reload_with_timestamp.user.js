2a7b6e774400ec1c61cec070e6f4a0ed39eab67f
bubbledheart <martin.mpeter@gmail.com>
2026-03-02 21:58:08 +0100

    Update youtube_reload_with_timestamp.user.js
    create svg manually and not within inner html element because of CSP

--- youtube_reload_with_timestamp.user.js
+++ youtube_reload_with_timestamp.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         youtube_reload_with_timestamp
-// @version      1.0
+// @version      1.1
 // @description  adds a button to youtube watch pages that reloads the page with the current timestamp added as a url parameter
 // @match        *.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
@@ -30,12 +30,8 @@
             margin-left: 10px;
             padding: 0;
         `;
-        button.innerHTML = `
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 7V12L15 14" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="gray" stroke-width="2"/>
-            </svg>
-        `;
+
+        button.appendChild(createSvg());
 
         button.onclick = function () {
             let video = document.querySelector("video");
@@ -49,6 +45,34 @@
         };
 
         return button;
+    }
+
+    function createSvg() {
+        let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("width", "20");
+        svg.setAttribute("height", "20");
+        svg.setAttribute("viewBox", "0 0 24 24");
+        svg.setAttribute("fill", "none");
+
+        let path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path1.setAttribute("d", "M12 7V12L15 14");
+        path1.setAttribute("stroke", "gray");
+        path1.setAttribute("stroke-width", "2");
+        path1.setAttribute("stroke-linecap", "round");
+        path1.setAttribute("stroke-linejoin", "round");
+
+        let path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path2.setAttribute(
+            "d",
+            "M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+        );
+        path2.setAttribute("stroke", "gray");
+        path2.setAttribute("stroke-width", "2");
+
+        svg.appendChild(path1);
+        svg.appendChild(path2);
+
+        return svg;
     }
 
     function addReloadButton() {
